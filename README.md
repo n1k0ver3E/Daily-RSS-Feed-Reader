f344afc314d32857b43356653b113d43fb22185d
pyf0311 <pyf0311@gmail.com>
2021-02-04 16:26:58 +0800

    Add resource/ and readme.md

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # Daily-RSS-Feed-Reader
-An alfred workflow to browse content of subscribed RSS feeds
+An alfred workflow to display content of RSS feeds Directly.
+
+## Install
+- Download `Daily.RSS.Feed.Reader.alfredworkflow` from [release](https://github.com/PYF0311/Daily-RSS-Feed-Reader/releases/tag/v1.0.0).
+- Double click to import.
+
+## Features
+- Directly display the content of RSS feeds.
+- Easy to manage RSS sources.
+
+## Instructions
+- ``rss`` : Display the contents from all RSS feeds
+
+  ![](/Users/Niko/Git/PYF0311/Daily-RSS-Feed-Reader/img/rss.gif)
+
+  
+
+- ``rss [source_name] `` : Display the contents from certain RSS feed. eg: rss sspai 
+
+  ![](/Users/Niko/Git/PYF0311/Daily-RSS-Feed-Reader/img/rss_sourceName.gif)
+
+- ``rss help`` : Display all available commands
+
+  ![](/Users/Niko/Git/PYF0311/Daily-RSS-Feed-Reader/img/rss_help.gif)
+
+- rss open : Open ``config.json`` to manage RSS feeds
+
+  ![](/Users/Niko/Git/PYF0311/Daily-RSS-Feed-Reader/img/rss_open.gif)
+
+
+
--- src/main.py
+++ src/main.py
@@ -1,40 +1,89 @@
 import sys
+
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 from workflow import Workflow, web, ICON_WARNING
 from datetime import datetime
 import json, time
-import sys,os
+import sys, os
 
-import urllib,requests
+import urllib, requests
 import xml.etree.ElementTree as ET
+
 
 class RSSParser(object):
     # def __init__(self,url):
     #     self.url = url
 
     def format_date(self, date_):
+        """pubdate formatter, parse format based on RSS 2.0 pubdate format.
+        parse format can be modified on variable rss_pubdate_fmt.
+
+        Args:
+            date_
+        Return:
+            date with new format
+        """
+
         rss_pubdate_fmt = "%a, %d %b %Y %H:%M:%S"
         new_date_fmt = "%Y-%m-%d %H:%M:%S"
         date_ = " ".join(date_.split(" ")[:-1])
         return datetime.strptime(date_, rss_pubdate_fmt).strftime(new_date_fmt)
 
     def return_rss_list(self):
-        """Return rssSubscribeList
-        :return: [{u'link': u'https://sspai.com/feed', u'title': u'sspai'}, {u'link': u'https://rsshub.app/ifanr/app', u'title': u'ifanr'}]
+        """Fetch List with title and link from config.json
+
+        Return:
+            List, rss_subscribe_list
+            for example:[{u'link': u'https://sspai.com/feed', u'title': u'sspai'}, {u'link': u'https://rsshub.app/ifanr/app', u'title': u'ifanr'}]
+
+        Raises:
+            Exception: Return False. An error occurred when read config.json
         """
-        
+
         with open('./config.json', 'r') as f:
             data = json.load(f)
             try:
-                rssSubscribeList = data[u'rss_subscribe']
-                # print("rssSubscribeList", rssSubscribeList)
-                return rssSubscribeList
+                rss_subscribe_list = data[u'rss_subscribe']
+                return rss_subscribe_list
             except Exception as e:
                 return False
-   
-    def parse_xml_text(self,source, url):
+
+    def return_xml_item(self):
+        """Fetch Final result list with title, link, source, date
+
+        Return:
+            result
+            for example: [{'date': '2021-02-04 09:07:33', 'source': u'sspai', 'link': 'https://sspai.com/post/64927', 'title': u'\u6d3e\u65e9'},{...}]
+
+        Raises:
+            Exception: Return False. An error occurred when parse xml from rss_subscribe_list.link
+        """
+
+        result = list()
+        rss_list = self.return_rss_list()
+        try:
+            for rss in rss_list:
+                rss_temp = self.parse_xml_text(rss[u'title'], rss[u'link'])
+                result = result + rss_temp
+            return result
+        except Exception as e:
+            return False
+
+    def parse_xml_text(self, source, url):
+        """parse xml content from certain source and save to list.
+
+        Args:
+            source: RSS source, read from config.json
+            url: RSS link, read from config.json
+
+
+        Return:
+            res
+            for example: [{'date': '2021-02-04 09:07:33', 'source': u'sspai', 'link': 'https://sspai.com/post/64927', 'title': u'\u6d3e\u65e9'},{...}]
+        """
+
         res = list()
         text = requests.get(url).content
         text = ET.fromstring(text)
@@ -46,127 +95,124 @@ class RSSParser(object):
             temp["link"] = item.findtext("link")
             res.append(temp)
         return res
-        
-    def return_xml_item(self):
-        result = list()
-        rssList = self.return_rss_list()   
-        try:
-            for rss in rssList:
-                # rssTemp = wf.cached_data('total', lambda: self.parse_xml_text(rss[u'title'], rss[u'link']), max_age=300)
-                rssTemp = self.parse_xml_text(rss[u'title'], rss[u'link'])
-                result = result + rssTemp
-            return result
-        except Exception as e:
-            return False
-        
-# def sorted_rssList_by_date():
-    
+
+
 def main(wf):
-    url = "https://www.ifanr.com/feed"
-    url = "https://rsshub.app/v2ex/topics/hot"
     arguments = wf.args
     # print(arguments)
-    args = ''.join(wf.args)
-    
+    args = ''.join(arguments)
+
     if args:
         args = wf.args[0].strip(" ").split(" ")
-        
+
         if len(args) == 1 and args[0] == u"help":
+            """
+            :Command: RSS help
+            """
             wf.add_item(
-                title = "rss",
-                subtitle = "Display contents from all RSS sources.",
-                icon = 'icon/ICON_INFO.png'
+                title="rss",
+                subtitle="Display contents from all RSS sources.",
+                icon='icon/ICON_INFO.png'
             )
             wf.add_item(
-                title = "rss list",
-                subtitle = "List all RSS sources.",
-                icon = 'icon/ICON_INFO.png'
+                title="rss list",
+                subtitle="List all RSS sources.",
+                icon='icon/ICON_INFO.png'
             )
             wf.add_item(
-                title = "rss open",
-                subtitle = "Open config.json to add new source.",
-                icon = 'icon/ICON_INFO.png',
-                valid = "yes",
-                arg = "open"
-                
+                title="rss open",
+                subtitle="Open config.json to add new source.",
+                icon='icon/ICON_INFO.png',
+                valid="yes",
+                arg="open"
+
             )
             wf.add_item(
-                title = "rss <source_name>  eg: rss sspai",
-                subtitle = "List contents of certain rss source.",
-                icon = 'icon/ICON_INFO.png'
+                title="rss <source_name>  eg: rss sspai",
+                subtitle="List contents of certain rss source.",
+                icon='icon/ICON_INFO.png'
             )
         elif len(args) == 1 and args[0] == "list":
+            """
+            :Command: RSS list
+            """
             rssParser = RSSParser()
             rssList = rssParser.return_rss_list()
             # print(rssList)
             for item in rssList:
                 wf.add_item(
-                    title = "Source: " + item[u'title'],
-                    subtitle = "Link: " + item[u'link'],
-                    valid = "",
-                    icon = "",
-                    arg = ""
-                    )
+                    title="Source: " + item[u'title'],
+                    subtitle="Link: " + item[u'link'],
+                    valid="",
+                    icon="",
+                    arg=""
+                )
         elif len(args) == 1 and args[0] == "open":
+            """
+            :Command: RSS open
+            """
             wf.add_item(
-                title = "Open config.json to add new source",
-                icon = 'icon/ICON_SYNC.png',
-                valid = "yes",
-                arg = "open"
-                
+                title="Open config.json to add new source",
+                icon='icon/ICON_SYNC.png',
+                valid="yes",
+                arg="open"
+
             )
-            
-            
+
+
         else:
-            rssParser = RSSParser()
-            rssList = rssParser.return_rss_list()
-            rssSource = [i[u'title'] for i in rssList]
+            rss_parser = RSSParser()
+            rss_list = rss_parser.return_rss_list()
+            rssSource = [i[u'title'] for i in rss_list]
             arg_source = arguments[0]
-            
+
             if arg_source in rssSource:
-                for i in rssList:
+                """
+                :Command: RSS <source_name>
+                """
+                for i in rss_list:
                     if i['title'] == arg_source:
-                        rss_url =  i['link']
-                result = rssParser.parse_xml_text(arg_source, rss_url)
-                # result = wf.cached_data('data', lambda: rssParser.parse_xml_text(arg_source, rss_url), max_age=300)
+                        rss_url = i['link']
+                result = rss_parser.parse_xml_text(arg_source, rss_url)
                 for item in result:
                     wf.add_item(
-                        title = item["title"],
-                        subtitle = "Source: " + item["source"] + "          PubDate: " + item["date"],
+                        title=item["title"],
+                        subtitle="Source: " + item["source"] + "          PubDate: " + item["date"],
                         valid='yes',
-                        arg = item["link"]
+                        arg=item["link"]
                     )
             else:
+                """
+                Catch command exception
+                """
                 wf.add_item(
-                    title = "RSS source not found at ./config.json",
-                    valid = "",
-                    icon = 'icon/ICON_ERROR.png',
-                    arg = ""
-                    )
-            
+                    title="RSS source not found at ./config.json",
+                    valid="",
+                    icon='icon/ICON_ERROR.png',
+                    arg=""
+                )
+
     else:
-        rssParser = RSSParser()
-        # result = rssParser.return_xml_item()
-        result = wf.cached_data('total', rssParser.return_xml_item, max_age=300)
-        # print("resultttttt:",result)
+        rss_parser = RSSParser()
+        result = wf.cached_data('total', rss_parser.return_xml_item, max_age=300)
         if result:
-            orderedResult = sorted(result,key=lambda x:x["date"],reverse=True)
-            for item in orderedResult:
+            ordered_result = sorted(result, key=lambda x: x["date"], reverse=True)
+            for item in ordered_result:
                 wf.add_item(
-                    title = item["title"],
-                    subtitle = "Source: " + item["source"] + "          PubDate: " + item["date"],
+                    title=item["title"],
+                    subtitle="Source: " + item["source"] + "          PubDate: " + item["date"],
                     valid='yes',
-                    arg = item["link"]
-                    )
+                    arg=item["link"]
+                )
         else:
             wf.add_item(
-                title = "SourceError : RSS source cannot be found or parsed successfully.",
-                subtitle = "Please input 'rss open' to check configuration."
-                )
+                title="SourceError : RSS source cannot be found or parsed successfully.",
+                subtitle="Please input 'rss open' to check configuration."
+            )
     wf.send_feedback()
+
 
 if __name__ == '__main__':
     wf = Workflow()
-    wf.settings['key111'] = {'key2': 'value'}
     log = wf.logger
     sys.exit(wf.run(main))
